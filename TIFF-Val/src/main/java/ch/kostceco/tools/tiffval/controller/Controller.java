63a21d867317d2bb8a4b7215e84373dc822c47f5
Chlara <chlara@gmx.ch>
2013-05-15 17:06:20 +0200

    2. Unnötige (SIP-Val-)Module entfernt ================================== Folgende (SIP-Val-)Module wurden entfernt: 1b 1c 3a 3b

--- TIFF-Val/src/main/java/ch/kostceco/tools/tiffval/controller/Controller.java
+++ TIFF-Val/src/main/java/ch/kostceco/tools/tiffval/controller/Controller.java
@@ -21,21 +21,13 @@ package ch.kostceco.tools.tiffval.controller;
 import java.io.File;
 
 import ch.kostceco.tools.tiffval.exception.module1.Validation1aZipException;
-import ch.kostceco.tools.tiffval.exception.module1.Validation1bFolderStructureException;
-import ch.kostceco.tools.tiffval.exception.module1.Validation1cNamingException;
 import ch.kostceco.tools.tiffval.exception.module1.Validation1dMetadataException;
-import ch.kostceco.tools.tiffval.exception.module3.Validation3aFormatRecognitionException;
-import ch.kostceco.tools.tiffval.exception.module3.Validation3bUnspecifiedFormatException;
 import ch.kostceco.tools.tiffval.exception.module3.Validation3cFormatValidationException;
 import ch.kostceco.tools.tiffval.logging.Logger;
 import ch.kostceco.tools.tiffval.logging.MessageConstants;
 import ch.kostceco.tools.tiffval.service.TextResourceService;
 import ch.kostceco.tools.tiffval.validation.module1.Validation1aZipModule;
-import ch.kostceco.tools.tiffval.validation.module1.Validation1bFolderStructureModule;
-import ch.kostceco.tools.tiffval.validation.module1.Validation1cNamingModule;
 import ch.kostceco.tools.tiffval.validation.module1.Validation1dMetadataModule;
-import ch.kostceco.tools.tiffval.validation.module3.Validation3aFormatRecognitionModule;
-import ch.kostceco.tools.tiffval.validation.module3.Validation3bUnspecifiedFormatModule;
 import ch.kostceco.tools.tiffval.validation.module3.Validation3cFormatValidationModule;
 
 /**
@@ -55,15 +47,7 @@ public class Controller implements MessageConstants
 
 	private Validation1aZipModule					validation1aZipModule;
 
-	private Validation1bFolderStructureModule		validation1bFolderStructureModule;
-
-	private Validation1cNamingModule				validation1cNamingModule;
-
 	private Validation1dMetadataModule				validation1dMetadataModule;
-
-	private Validation3aFormatRecognitionModule		validation3aFormatRecognitionModule;
-
-	private Validation3bUnspecifiedFormatModule		validation3bUnspecifiedFormatModule;
 
 	private Validation3cFormatValidationModule		validation3cFormatValidationModule;
 
@@ -82,28 +66,6 @@ public class Controller implements MessageConstants
 		this.validation1aZipModule = validation1aZipModule;
 	}
 
-	public Validation1bFolderStructureModule getValidation1bFolderStructureModule()
-	{
-		return validation1bFolderStructureModule;
-	}
-
-	public void setValidation1bFolderStructureModule(
-			Validation1bFolderStructureModule validation1bFolderStructureModule )
-	{
-		this.validation1bFolderStructureModule = validation1bFolderStructureModule;
-	}
-
-	public Validation1cNamingModule getValidation1cNamingModule()
-	{
-		return validation1cNamingModule;
-	}
-
-	public void setValidation1cNamingModule(
-			Validation1cNamingModule validation1cNamingModule )
-	{
-		this.validation1cNamingModule = validation1cNamingModule;
-	}
-
 	public Validation1dMetadataModule getValidation1dMetadataModule()
 	{
 		return validation1dMetadataModule;
@@ -113,28 +75,6 @@ public class Controller implements MessageConstants
 			Validation1dMetadataModule validation1dMetadataModule )
 	{
 		this.validation1dMetadataModule = validation1dMetadataModule;
-	}
-
-	public Validation3aFormatRecognitionModule getValidation3aFormatRecognitionModule()
-	{
-		return validation3aFormatRecognitionModule;
-	}
-
-	public void setValidation3aFormatRecognitionModule(
-			Validation3aFormatRecognitionModule validation3aFormatRecognitionModule )
-	{
-		this.validation3aFormatRecognitionModule = validation3aFormatRecognitionModule;
-	}
-
-	public Validation3bUnspecifiedFormatModule getValidation3bUnspecifiedFormatModule()
-	{
-		return validation3bUnspecifiedFormatModule;
-	}
-
-	public void setValidation3bUnspecifiedFormatModule(
-			Validation3bUnspecifiedFormatModule validation3bUnspecifiedFormatModule )
-	{
-		this.validation3bUnspecifiedFormatModule = validation3bUnspecifiedFormatModule;
 	}
 
 	public Validation3cFormatValidationModule getValidation3cFormatValidationModule()
@@ -196,71 +136,6 @@ public class Controller implements MessageConstants
 			return false;
 		}
 
-		// Validation Step Ab
-		try {
-			if ( this.getValidation1bFolderStructureModule()
-					.validate( sipDatei ) ) {
-				LOGGER.logInfo( getTextResourceService().getText(
-						MESSAGE_MODULE_VALID,
-						getTextResourceService().getText( MESSAGE_MODULE_Ab ) ) );
-				this.getValidation1bFolderStructureModule().getMessageService()
-						.print();
-			} else {
-				LOGGER.logInfo( getTextResourceService().getText(
-						MESSAGE_MODULE_INVALID,
-						getTextResourceService().getText( MESSAGE_MODULE_Ab ) )
-						+ getTextResourceService().getText(
-								MESSAGE_STEPERGEBNIS_Ab ) );
-				// Ein negatives Validierungsresultat in diesem Schritt führt
-				// zum Abbruch der weiteren Verarbeitung
-				this.getValidation1bFolderStructureModule().getMessageService()
-						.print();
-				return false;
-			}
-		} catch ( Validation1bFolderStructureException e ) {
-			LOGGER.logInfo( getTextResourceService().getText(
-					MESSAGE_MODULE_INVALID_2ARGS,
-					getTextResourceService().getText( MESSAGE_MODULE_Ab ),
-					e.getMessage() ) );
-			this.getValidation1bFolderStructureModule().getMessageService()
-					.print();
-			return false;
-		} catch ( Exception e ) {
-			LOGGER.logInfo( getTextResourceService().getText( ERROR_UNKNOWN ) );
-			LOGGER.logError( e.getMessage() );
-			return false;
-		}
-
-		// Validation Step Ac
-		try {
-			if ( this.getValidation1cNamingModule().validate( sipDatei ) ) {
-				LOGGER.logInfo( getTextResourceService().getText(
-						MESSAGE_MODULE_VALID,
-						getTextResourceService().getText( MESSAGE_MODULE_Ac ) ) );
-				this.getValidation1cNamingModule().getMessageService().print();
-			} else {
-				LOGGER.logInfo( getTextResourceService().getText(
-						MESSAGE_MODULE_INVALID,
-						getTextResourceService().getText( MESSAGE_MODULE_Ac ) )
-						+ getTextResourceService().getText(
-								MESSAGE_STEPERGEBNIS_Ac ) );
-				this.getValidation1cNamingModule().getMessageService().print();
-				// Ein negatives Validierungsresultat in diesem Schritt führt
-				// zum Abbruch der weiteren Verarbeitung
-				return false;
-			}
-		} catch ( Validation1cNamingException e ) {
-			LOGGER.logInfo( getTextResourceService().getText(
-					MESSAGE_MODULE_INVALID_2ARGS,
-					getTextResourceService().getText( MESSAGE_MODULE_Ac ),
-					e.getMessage() ) );
-			this.getValidation1cNamingModule().getMessageService().print();
-			return false;
-		} catch ( Exception e ) {
-			LOGGER.logInfo( getTextResourceService().getText( ERROR_UNKNOWN ) );
-			LOGGER.logError( e.getMessage() );
-			return false;
-		}
 
 		// Validation Step Ad
 		try {
@@ -303,77 +178,7 @@ public class Controller implements MessageConstants
 	{
 		boolean valid = true;
 
-
-
-
-		// Validation Step Ca
-		try {
-			if ( this.getValidation3aFormatRecognitionModule().validate(
-					sipDatei ) ) {
-				LOGGER.logInfo( getTextResourceService().getText(
-						MESSAGE_MODULE_VALID,
-						getTextResourceService().getText( MESSAGE_MODULE_Ca ) ) );
-				this.getValidation3aFormatRecognitionModule()
-						.getMessageService().print();
-			} else {
-				LOGGER.logInfo( getTextResourceService().getText(
-						MESSAGE_MODULE_INVALID,
-						getTextResourceService().getText( MESSAGE_MODULE_Ca ) )
-						+ getTextResourceService().getText(
-								MESSAGE_STEPERGEBNIS_Ca ) );
-				this.getValidation3aFormatRecognitionModule()
-						.getMessageService().print();
-				valid = false;
-			}
-		} catch ( Validation3aFormatRecognitionException e ) {
-			LOGGER.logInfo( getTextResourceService().getText(
-					MESSAGE_MODULE_INVALID_2ARGS,
-					getTextResourceService().getText( MESSAGE_MODULE_Ca ),
-					e.getMessage() ) );
-			this.getValidation3aFormatRecognitionModule().getMessageService()
-					.print();
-			valid = false;
-		} catch ( Exception e ) {
-			LOGGER.logInfo( getTextResourceService().getText( ERROR_UNKNOWN ) );
-			LOGGER.logError( e.getMessage() );
-			return false;
-		}
-
-		// Validation Step Cb
-		try {
-			if ( this.getValidation3bUnspecifiedFormatModule().validate(
-					sipDatei ) ) {
-				LOGGER.logInfo( getTextResourceService().getText(
-						MESSAGE_MODULE_VALID,
-						getTextResourceService().getText( MESSAGE_MODULE_Cb ) ) );
-				this.getValidation3bUnspecifiedFormatModule()
-						.getMessageService().print();
-			} else {
-				LOGGER.logInfo( getTextResourceService().getText(
-						MESSAGE_MODULE_INVALID,
-						getTextResourceService().getText( MESSAGE_MODULE_Cb ) )
-						+ getTextResourceService().getText(
-								MESSAGE_STEPERGEBNIS_Cb ) );
-				valid = false;
-				this.getValidation3bUnspecifiedFormatModule()
-						.getMessageService().print();
-			}
-		} catch ( Validation3bUnspecifiedFormatException e ) {
-			LOGGER.logInfo( getTextResourceService().getText(
-					MESSAGE_MODULE_INVALID_2ARGS,
-					getTextResourceService().getText( MESSAGE_MODULE_Cb ),
-					e.getMessage() ) );
-			this.getValidation3bUnspecifiedFormatModule().getMessageService()
-					.print();
-			valid = false;
-		} catch ( Exception e ) {
-			LOGGER.logInfo( getTextResourceService().getText( ERROR_UNKNOWN ) );
-			LOGGER.logError( e.getMessage() );
-			return false;
-		}
-
 		return valid;
-
 	}
 
 	public boolean execute3c( File sipDatei )
