609838c80aa9936a9fec0ba9adf274d94e2649d9
X60014195 <X60014195@CM032409.adb.intra.admin.ch>
2017-11-01 10:25:42 +0100

    Aktualisierung

--- KOST-Val/src/main/java/ch/kostceco/tools/kostval/KOSTVal.java
+++ KOST-Val/src/main/java/ch/kostceco/tools/kostval/KOSTVal.java
@@ -26,6 +26,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
+
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
